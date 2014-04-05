4c28a48f3f9befda3c5b9d4f7751d228181d6349
Natuu <natyouu@gmail.com>
2014-04-05 16:41:09 +0200

    Ajout du js des options et des couleurs

--- js/code.js
+++ js/code.js
@@ -10,7 +10,28 @@ $(function () {
 	$('.moins').click(function(){
 		var score = $(this).parent().parent().children('.score').text();
 		score --;
+
+		if (score < 0)
+		{
+			score = 0;
+		}
+
 		$(this).parent().parent().children('.score').text(score);
 	});
 
+
+	// Fonction de couleur
+	$('.color-pickable').click(function() {
+		var color = $(this).data('color'),
+			initalColor = $(this).parent().parent().parent().parent().parent().data('color');
+
+		$(this).parent().parent().parent().parent().parent().removeClass(initalColor).addClass(color).data('color', color);		
+	});
+
+
+	//Fonction d'affichage des equipes
+	$('.joueurs').click(function(){
+		$(this).parent().parent().parent().children('.index').toggle();
+		$(this).parent().parent().parent().children('.options').toggle();
+	});
 });
